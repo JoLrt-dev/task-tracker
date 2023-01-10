04e5577e45a070f98d88c3495c4975266afbf004
Jordane LAURENT <jordane.laurent_student2021@wilder.school>
2023-01-10 15:45:26 +0100

    task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task'; // interface
+import { TASKS } from 'src/app/mock-tasks'; // mock data
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS; // we want to pass the mock data as props of our Task component
+  constructor() {}
 
+  ngOnInit(): void {}
 }
