9870341f269303a471a42c392e7ae19831a2077b
Jordane LAURENT <jordane.laurent_student2021@wilder.school>
2023-01-12 09:55:16 +0100

    Btn color & text displays depends on showAddTask value

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs'; // Subscription is a representation of any work that can be cancelled, such as an Observable execution or an array of Subscriptions.
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false; //
+  subscription: Subscription;
 
-  constructor() {}
+  // change the value of showAddTask to the value of the subject
+
+  // The constructor() is where we inject the UiService.
+  // When you want to use a service you have to inject it into the constructor.
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => {
+      this.showAddTask = value; // value is gonna be the opposite of whatever showaddtask is
+    });
+  }
 
   ngOnInit(): void {}
-
+  // When we click on the "Add" we're calling the toggleAddTask() in our service
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
---
+++ src/app/services/ui.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UiService {
+  private showAddTask: boolean = false;
+  private subject = new Subject<any>(); // Subject is a special type of Observable that allows values to be multicasted to many Observers. Subjects are like EventEmitters: they maintain a registry of many listeners.
+
+  constructor() {}
+
+  toggleAddTask(): void {
+    // toggleAddTask() is called from the header component . This is the function that toggles the add-task component, and it also emits the value to the subject.
+    // Whatever the status is true or false, it will toggle it to the opposite value to display/hide the add-task component.
+    this.showAddTask = !this.showAddTask;
+    this.subject.next(this.showAddTask);
+  }
+
+  onToggle(): Observable<any> {
+    // onToggle() is called from the add-task component. It is the function that subscribes to the subject and returns the value to the add-task component.
+    return this.subject.asObservable();
+  }
+}
